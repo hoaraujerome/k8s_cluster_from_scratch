c53f22f7418f5c269ec6bab3ebf9ed80417f3f0e
Jay <jayrome82@gmail.com>
2023-08-19 09:33:09 -0400

    feat: install precommit hooks

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from imports.aws.provider import AwsProvider
 from imports.aws.vpc import Vpc
 from imports.aws.subnet import Subnet
 
+
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
@@ -12,21 +13,30 @@ class MyStack(TerraformStack):
         # define resources here
         AwsProvider(self, "AWS", region="ca-central-1")
 
-        aws_vpc_main = Vpc(self,
+        aws_vpc_main = Vpc(
+            self,
             "vpc",
-            cidr_block="10.0.0.0/16", # https://cidr.xyz
-            enable_dns_support=True, # Instances in the VPC can use Amazon-provided DNS server
-            enable_dns_hostnames=True, # Instances in the VPC will be assigned public DNS hostnames if they have public IP addresses
+            # https://cidr.xyz
+            cidr_block="10.0.0.0/16",
+            # Instances in the VPC can use Amazon-provided DNS server
+            enable_dns_support=True,
+            # Instances in the VPC will be assigned public DNS hostnames
+            # if they have public IP addresses
+            enable_dns_hostnames=True,
             tags={
                 "Name": "k8s-from-scratch-vpc"
             }
         )
 
         """
-        Private subnets to create a range of IP addresses that we can allocate to our instances which do not allow external access (unless through a proxy or load balancer):
-        for both the control plane controllers, as well as our worker instances
+        Private subnets to create a range of IP addresses that we can allocate
+        to our instances which do not allow external access (unless through a
+        proxy or load balancer): used for both the control plane controllers
+        as well as our worker instances
         """
-        Subnet(self, "private-subnet",
+        Subnet(
+            self,
+            "private-subnet",
             cidr_block="10.0.1.0/24",
             tags={
                 "Name": "k8s-from-scratch-private-subnet"
