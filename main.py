91a7618fdc749e1088f12bcb71a905e380fe9b23
Jay <jayrome82@gmail.com>
2023-09-04 21:02:03 -0400

    feat: prepare K8S control plane nodes

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 from cdktf import App
 from networking_stack import NetworkingStack, NetworkingStackConfig
 from load_balancer_stack import LoadBalancerStack, LoadBalancerStackConfig
+from kubernetes_controlplane_stack import (
+  KubernetesControlPlaneStack,
+  KubernetesControlPlaneStackConfig
+)
 
 
 TAG_NAME_PREFIX = "k8s-from-scratch-"
 
 app = App()
+
 networking_stack = NetworkingStack(
     app, "NetworkingStack",
     NetworkingStackConfig(
@@ -23,4 +28,14 @@ load_balancer_stack = LoadBalancerStack(
         subnet_id=networking_stack.aws_private_subnet.id
     ),
 )
+
+kubernetes_controlplane_stack = KubernetesControlPlaneStack(
+  app, "KubernetesControlPlaneStack",
+  KubernetesControlPlaneStackConfig(
+    tag_name_prefix=TAG_NAME_PREFIX,
+    region=networking_stack.region,
+    subnet_id=networking_stack.aws_private_subnet.id,
+  )
+)
+
 app.synth()
