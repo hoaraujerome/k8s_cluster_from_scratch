1dc96762f170008a161978a3bc8b2fa2e5615e21
Jay <jayrome82@gmail.com>
2024-09-10 22:29:31 -0400

    chore: clean up

--- provisioning/main-account/ca-central-1/beta/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "kubernetes-the-hard-way-on-aws"
-    key    = "tfstate/main-account/ca-central-1/beta"
-    region = "ca-central-1"
-  }
-}
--- provisioning/main-account/ca-central-1/beta/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  tag_prefix                         = "k8s-the-hard-way-"
-  vpc_ipv4_cidr_block                = "10.1.0.0/16"
-  k8s_cluster_subnet_ipv4_cidr_block = "10.1.1.0/24"
-}
