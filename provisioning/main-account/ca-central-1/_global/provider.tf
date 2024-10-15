df142aa159eec388bef3741ef41400de25ec3d27
Jay <jayrome82@gmail.com>
2024-10-14 20:47:40 -0400

    fix: terraform warnings

--- provisioning/main-account/ca-central-1/_global/provider.tf
+++ provisioning/main-account/ca-central-1/_global/provider.tf
@@ -1,8 +1,15 @@
 terraform {
+  required_version = "~> 1.8.5"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.53.0"
+    }
+
+    external = {
+      source  = "hashicorp/external"
+      version = "2.3.3"
     }
   }
 }
